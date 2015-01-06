076bfef95d2c5313e641004b382f988e8fc2ff5e
George Moromisato <public@neurohack.com>
2015-01-05 20:47:05 -0800

    Initial commit

---
+++ TSE/PreComp.h
@@ -0,0 +1,14 @@
+//	PreComp.h
+
+#include <windows.h>
+#include <ddraw.h>
+#include "Alchemy.h"
+#include "DirectXUtil.h"
+#include "TSE.h"
+
+#include "TSESFXImpl.h"
+#include "TSEDeviceClassesImpl.h"
+#include "TSESpaceObjectsImpl.h"
+#include "TSEShipControllersImpl.h"
+#include "TSEOrderModuleImpl.h"
+#include "TSETopologyProcessorsImpl.h"
