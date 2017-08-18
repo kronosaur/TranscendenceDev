215628af0b82b39c76e712fefce968f9d9f2bb7a
George Moromisato <public@neurohack.com>
2017-08-17 17:18:39 -0700

    Added unique pointer template

--- Include/TSmartPtr.h
+++ Include/TSmartPtr.h
@@ -68,3 +68,109 @@ template <class OBJ> class TSharedPtr
 	private:
 		OBJ *m_pPtr;
 	};
+
+template <class OBJ> class TUniquePtr
+	{
+	public:
+		constexpr TUniquePtr (void) : m_pPtr(NULL) { }
+		constexpr TUniquePtr (std::nullptr_t) : m_pPtr(NULL) { }
+
+		explicit TUniquePtr (OBJ *pPtr) : m_pPtr(pPtr) { }
+
+		TUniquePtr (const TUniquePtr<OBJ> &Src)
+			{
+			if (Src.m_pPtr)
+				m_pPtr = new OBJ(*Src.m_pPtr);
+			else
+				m_pPtr = NULL;
+			}
+
+		TUniquePtr (TUniquePtr<OBJ> &&Src) : m_pPtr(Src.m_pPtr)
+			{
+			Src.m_pPtr = NULL;
+			}
+
+		~TUniquePtr (void)
+			{
+			if (m_pPtr)
+				delete m_pPtr;
+			}
+
+		TUniquePtr<OBJ> &operator= (const TUniquePtr<OBJ> &Src)
+			{
+			if (m_pPtr)
+				delete m_pPtr;
+
+			if (Src.m_pPtr)
+				m_pPtr = new OBJ(*Src.m_pPtr);
+			else
+				m_pPtr = NULL;
+
+			return *this;
+			}
+
+		operator OBJ *() const { return m_pPtr; }
+		OBJ * operator->() const { return m_pPtr; }
+
+		explicit operator bool() const { return (m_pPtr != NULL); }
+
+		void Delete (void) { Set(NULL); }
+
+		void Set (OBJ *pPtr)
+			{
+			if (m_pPtr)
+				delete m_pPtr;
+
+			m_pPtr = pPtr;
+			}
+
+		void Set (const TUniquePtr<OBJ> &Src)
+			{
+			*this = Src;
+			}
+
+	private:
+		OBJ *m_pPtr;
+	};
+
+template <class OBJ> class TUniquePtr<OBJ[]>
+	{
+	public:
+		constexpr TUniquePtr (void) : m_pPtr(NULL) { }
+		constexpr TUniquePtr (std::nullptr_t) : m_pPtr(NULL) { }
+
+		explicit TUniquePtr (OBJ *pPtr) : m_pPtr(pPtr) { }
+
+		TUniquePtr (const TUniquePtr<OBJ> &Src) =delete;
+
+		TUniquePtr (TUniquePtr<OBJ> &&Src) : m_pPtr(Src.m_pPtr)
+			{
+			Src.m_pPtr = NULL;
+			}
+
+		~TUniquePtr (void)
+			{
+			if (m_pPtr)
+				delete [] m_pPtr;
+			}
+
+		TUniquePtr<OBJ> &operator= (const TUniquePtr<OBJ> &Src) =delete;
+		TUniquePtr<OBJ[]> &operator= (const TUniquePtr<OBJ[]> &Src) =delete;
+
+		OBJ & operator[](size_t i) const { return m_pPtr[i]; }
+
+		explicit operator bool() const { return (m_pPtr != NULL); }
+
+		void Delete (void) { Set(NULL); }
+
+		void Set (OBJ *pPtr)
+			{
+			if (m_pPtr)
+				delete [] m_pPtr;
+
+			m_pPtr = pPtr;
+			}
+
+	private:
+		OBJ *m_pPtr;
+	};
