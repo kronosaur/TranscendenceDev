c18e9576aa19eb791cd4f2ec455e2747f8a08dd5
George Moromisato <public@neurohack.com>
2018-12-03 17:37:49 -0800

    Fixes for item selectors

--- Include/TSEDockScreenSession.h
+++ Include/TSEDockScreenSession.h
@@ -14,6 +14,7 @@ class IDockScreenUI
 
 			selOriginal,					//	Select the original item
 			selModified,					//	Select the modified item
+			selReset,						//	Reset the list
 			};
 
 		struct SModifyItemCtx
