0cb05174aa57d3f1ba699e1f0f9677e4f2686222
George Moromisato <public@neurohack.com>
2018-12-10 11:23:56 -0800

    Fix corruption caused by previous version

--- TSE/CUniverse.cpp
+++ TSE/CUniverse.cpp
@@ -2055,29 +2055,32 @@ ALERROR CUniverse::LoadFromStream (IReadStream *pStream, DWORD *retdwSystemID, D
 			CDesignType *pType = FindDesignType(dwLoad);
 			if (pType == NULL)
 				{
-#ifdef DEBUG_FILE_CORRUPTION
-				i++;
-				if (i < (int)dwCount)
+				if (Ctx.dwSystemVersion < 168 && dwLoad == 0x9101d5)
 					{
-					do
+					i++;
+					if (i < (int)dwCount)
 						{
-						pStream->Read((char *)&dwLoad, sizeof(DWORD));
-						pType = FindDesignType(dwLoad);
+						do
+							{
+							pStream->Read((char *)&dwLoad, sizeof(DWORD));
+							pType = FindDesignType(dwLoad);
+							}
+						while (pType == NULL);
 						}
-					while (pType == NULL);
-					}
 
-				if (pType == NULL)
+					if (pType == NULL)
+						{
+						*retsError = strPatternSubst(CONSTLIT("Unable to find type: %x (missing mod?)"), dwLoad);
+						return ERR_FAIL;
+						}
+
+					//	fall through
+					}
+				else
 					{
 					*retsError = strPatternSubst(CONSTLIT("Unable to find type: %x (missing mod?)"), dwLoad);
 					return ERR_FAIL;
 					}
-
-				//	fall through
-#else
-				*retsError = strPatternSubst(CONSTLIT("Unable to find type: %x (missing mod?)"), dwLoad);
-				return ERR_FAIL;
-#endif
 				}
 
 			try
