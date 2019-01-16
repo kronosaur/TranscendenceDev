0f6f6ff940b64fbe1ed8ce77ea7163f87e8d27ac
George Moromisato <public@neurohack.com>
2019-01-16 10:44:03 -0800

    Batch files

--- Transcendence/Game/compileDebug.bat
+++
@@ -1,2 +0,0 @@
-@rem Compiles Debug Extensions
-@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\Debug\DebugPart1.xml" /output:TDBs\DebugPart1.tdb
---
+++ Transcendence/Game/compileDebug1.bat
@@ -0,0 +1,2 @@
+@rem Compiles Debug Part 1
+@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\DebugPart1\DebugPart1.xml" /output:TDBs\DebugPart1.tdb
