51f74fc406ac95c8070bc93d7403128ce7ae76b4
George Moromisato <public@neurohack.com>
2019-06-17 16:10:00 -0700

    Fix crash

--- Mammoth/TSE/CShip.cpp
+++ Mammoth/TSE/CShip.cpp
@@ -4403,7 +4403,7 @@ EDamageResults CShip::OnDamage (SDamageCtx &Ctx)
 
 	//	Short-circuit
 
-	if (Ctx.iDamage == 0)
+	if (Ctx.iDamage == 0 || GetSystem() == NULL)
 		return damageNoDamage;
 
 	bool bIsPlayer = IsPlayer();
