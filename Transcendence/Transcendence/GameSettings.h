65653bdec929f3fd7a9e7278392f48f9ed46c03f
George Moromisato <public@neurohack.com>
2020-01-06 11:36:53 -0800

    Clean up

--- Transcendence/Transcendence/GameSettings.h
+++ Transcendence/Transcendence/GameSettings.h
@@ -136,13 +136,13 @@ class CGameKeys
 		CGameKeys (void);
 
         void GetCommands (TArray<SCommandKeyDesc> &Result) const;
-		inline Keys GetGameCommand (DWORD dwVirtKey) const { return m_iMap[(dwVirtKey < 256 ? dwVirtKey : 0)]; }
+		Keys GetGameCommand (DWORD dwVirtKey) const { return m_iMap[(dwVirtKey < 256 ? dwVirtKey : 0)]; }
         Keys GetGameCommandFromChar (char chChar) const;
 		char GetKeyIfChar (Keys iCommand) const;
         DWORD GetKey (Keys iCommand) const;
-        inline ELayouts GetLayout (void) const { return m_iLayout; }
+        ELayouts GetLayout (void) const { return m_iLayout; }
         CString GetLayoutName (ELayouts iLayout) const;
-        inline bool IsModified (void) const { return m_bModified; }
+        bool IsModified (void) const { return m_bModified; }
 		bool IsNonRepeatCommand (Keys iCommand) const;
 		bool IsStatefulCommand (Keys iCommand) const;
 		ALERROR ReadFromXML (CXMLElement *pDesc);
@@ -256,27 +256,27 @@ class CGameSettings
 
 		CGameSettings (IExtraSettingsHandler *pExtra = NULL) : m_pExtra(pExtra) { }
 
-		inline const CString &GetAppDataFolder (void) const { return m_sAppData; }
-		inline bool GetBoolean (int iOption) const { return m_Options[iOption].bValue; }
-		inline void GetDefaultExtensions (DWORD dwAdventure, bool bDebugMode, TArray<DWORD> *retList) const { m_Extensions.GetList(dwAdventure, bDebugMode, retList); }
-		inline void GetDefaultExtensions (DWORD dwAdventure, const TArray<CExtension *> &Available, bool bDebugMode, TArray<DWORD> *retList) const { m_Extensions.GetList(dwAdventure, Available, bDebugMode, retList); }
-		inline const TSortMap<DWORD, bool> &GetDisabledExtensionList (void) const { return m_Extensions.GetDisabledExtensionList(); }
-		inline const TArray<CString> &GetExtensionFolders (void) const { return m_ExtensionFolders; }
-		inline const CString &GetInitialSaveFile (void) const { return m_sSaveFile; }
-		inline int GetInteger (int iOption) const { return m_Options[iOption].iValue; }
-		inline CGameKeys &GetKeyMap (void) { return m_KeyMap; }
-		inline const CGameKeys &GetKeyMap (void) const { return m_KeyMap; }
-		inline const CString &GetString (int iOption) const { return m_Options[iOption].sValue; }
+		const CString &GetAppDataFolder (void) const { return m_sAppData; }
+		bool GetBoolean (int iOption) const { return m_Options[iOption].bValue; }
+		void GetDefaultExtensions (DWORD dwAdventure, bool bDebugMode, TArray<DWORD> *retList) const { m_Extensions.GetList(dwAdventure, bDebugMode, retList); }
+		void GetDefaultExtensions (DWORD dwAdventure, const TArray<CExtension *> &Available, bool bDebugMode, TArray<DWORD> *retList) const { m_Extensions.GetList(dwAdventure, Available, bDebugMode, retList); }
+		const TSortMap<DWORD, bool> &GetDisabledExtensionList (void) const { return m_Extensions.GetDisabledExtensionList(); }
+		const TArray<CString> &GetExtensionFolders (void) const { return m_ExtensionFolders; }
+		const CString &GetInitialSaveFile (void) const { return m_sSaveFile; }
+		int GetInteger (int iOption) const { return m_Options[iOption].iValue; }
+		CGameKeys &GetKeyMap (void) { return m_KeyMap; }
+		const CGameKeys &GetKeyMap (void) const { return m_KeyMap; }
+		const CString &GetString (int iOption) const { return m_Options[iOption].sValue; }
 		ALERROR Load (const CString &sFilespec, CString *retsError = NULL);
 		ALERROR ParseCommandLine (char *pszCmdLine);
 		ALERROR Save (const CString &sFilespec);
-		inline void SetBoolean (int iOption, bool bValue, bool bModifySettings = true) { SetValueBoolean(iOption, bValue, bModifySettings); if (bModifySettings) m_bModified = true; }
-		inline void SetDefaultExtensions (DWORD dwAdventure, const TArray<CExtension *> &Available, bool bDebugMode, const TArray<DWORD> &List) { m_Extensions.SetList(dwAdventure, Available, bDebugMode, List); m_bModified = true; }
-		inline void SetExtensionEnabled (DWORD dwUNID, bool bEnabled = true) { m_Extensions.SetExtensionEnabled(dwUNID, bEnabled); m_bModified = true; }
-		inline void SetInteger (int iOption, int iValue, bool bModifySettings = true) { SetValueInteger(iOption, iValue, bModifySettings); if (bModifySettings) m_bModified = true; }
-		inline void SetModified (void) { m_bModified = true; }
-		inline void SetSettingsHandler (IExtraSettingsHandler *pExtra) { m_pExtra = pExtra; }
-		inline void SetString (int iOption, const CString &sValue, bool bModifySettings = true) { SetValueString(iOption, sValue, bModifySettings); if (bModifySettings) m_bModified = true; }
+		void SetBoolean (int iOption, bool bValue, bool bModifySettings = true) { SetValueBoolean(iOption, bValue, bModifySettings); if (bModifySettings) m_bModified = true; }
+		void SetDefaultExtensions (DWORD dwAdventure, const TArray<CExtension *> &Available, bool bDebugMode, const TArray<DWORD> &List) { m_Extensions.SetList(dwAdventure, Available, bDebugMode, List); m_bModified = true; }
+		void SetExtensionEnabled (DWORD dwUNID, bool bEnabled = true) { m_Extensions.SetExtensionEnabled(dwUNID, bEnabled); m_bModified = true; }
+		void SetInteger (int iOption, int iValue, bool bModifySettings = true) { SetValueInteger(iOption, iValue, bModifySettings); if (bModifySettings) m_bModified = true; }
+		void SetModified (void) { m_bModified = true; }
+		void SetSettingsHandler (IExtraSettingsHandler *pExtra) { m_pExtra = pExtra; }
+		void SetString (int iOption, const CString &sValue, bool bModifySettings = true) { SetValueString(iOption, sValue, bModifySettings); if (bModifySettings) m_bModified = true; }
 
 	private:
 		struct SOption
