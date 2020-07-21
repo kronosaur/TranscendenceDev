b9710a754dc0563ba1e20926e70e93d375669d2a
Bryon Leung <teslaslegacy@gmail.com>
2020-07-20 23:58:59 -0400

    Fix OpenGL includes...possible kludge

--- Alchemy/Include/OpenGLIncludes.h
+++ Alchemy/Include/OpenGLIncludes.h
@@ -2,7 +2,7 @@
 
 #include <GL/glew.h>
 #include <GL/wglew.h>
-#pragma comment(lib, "glew32.lib")
+//#pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "opengl32.lib")
 
 #include <glm/glm.hpp>
