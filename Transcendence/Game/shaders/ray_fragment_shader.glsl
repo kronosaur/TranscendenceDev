dd1f8a42d714db0ceaee0cb078d0955c06aaf449
Bryon Leung <teslaslegacy@gmail.com>
2020-08-05 02:56:05 -0400

    Fix alpha issue with lightning effect

--- Transcendence/Game/shaders/ray_fragment_shader.glsl
+++ Transcendence/Game/shaders/ray_fragment_shader.glsl
@@ -68,74 +68,6 @@ vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
 vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
 vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
 
-float cnoise(vec3 P){
-  vec3 Pi0 = floor(P); // Integer part for indexing
-  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
-  Pi0 = mod(Pi0, 289.0);
-  Pi1 = mod(Pi1, 289.0);
-  vec3 Pf0 = fract(P); // Fractional part for interpolation
-  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
-  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
-  vec4 iy = vec4(Pi0.yy, Pi1.yy);
-  vec4 iz0 = Pi0.zzzz;
-  vec4 iz1 = Pi1.zzzz;
-
-  vec4 ixy = permute(permute(ix) + iy);
-  vec4 ixy0 = permute(ixy + iz0);
-  vec4 ixy1 = permute(ixy + iz1);
-
-  vec4 gx0 = ixy0 / 7.0;
-  vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
-  gx0 = fract(gx0);
-  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
-  vec4 sz0 = step(gz0, vec4(0.0));
-  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
-
-  vec4 gx1 = ixy1 / 7.0;
-  vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
-  gx1 = fract(gx1);
-  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-  vec4 sz1 = step(gz1, vec4(0.0));
-  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-
-  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-
-  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-  g000 *= norm0.x;
-  g010 *= norm0.y;
-  g100 *= norm0.z;
-  g110 *= norm0.w;
-  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-  g001 *= norm1.x;
-  g011 *= norm1.y;
-  g101 *= norm1.z;
-  g111 *= norm1.w;
-
-  float n000 = dot(g000, Pf0);
-  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-  float n111 = dot(g111, Pf1);
-
-  vec3 fade_xyz = fade(Pf0);
-  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
-  return 2.2 * n_xyz;
-}
-
 float rand(vec2 co){
   // Canonical PRNG from https://stackoverflow.com/questions/12964279/whats-the-origin-of-this-glsl-rand-one-liner
   return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
@@ -359,10 +291,6 @@ float sampleNoiseFBM(vec3 sampler) {
     return texture(perlin_noise, vec3(sampler[0], sampler[1], sampler[2]))[0];
 }
 
-float sampleNoiseFBMLightning(vec3 sampler) {
-    return texture(perlin_noise, vec3(sampler[0], sampler[1], sampler[2]))[2];
-}
-
 float sampleNoisePerlin(vec3 sampler) {
     //return texture(perlin_noise, vec3(sampler[0], sampler[1], abs(mod(sampler[2], 2.0) - 1.0)))[1];
     return texture(perlin_noise, vec3(sampler[0], sampler[1], sampler[2]))[1];
@@ -371,22 +299,6 @@ float sampleNoisePerlin(vec3 sampler) {
 float fbm(vec2 a, float b) {
     return (sampleNoiseFBM(vec3(a.x, a.y, b)) * 2.0) - 1.0;
 }
-
-float fbmLightning(vec2 a, float b) {
-    return (sampleNoiseFBMLightning(vec3(a.x, a.y, b)) * 2.0) - 1.0;
-}
-
-//float fbm(vec2 p, float time)
-//{
-     //float v = 0.0;
-     //float amp = 0.75;
-     //float freq = 1;
-     //int octaves = 2;
-     //for (int i = 0; i < octaves; i++)
-         //v += cnoise(vec3((p * (i * freq)), time)) * (amp / (i + 1));
-
-     //return v;
-//}
 
 vec4 calcRayColor(float taperAdjTop, float taperAdjBottom, float widthAdjTop, float widthAdjBottom, float center_point, vec2 real_texcoord, float intensity, float distanceFromCenter, float grains)
 {
@@ -427,7 +339,7 @@ vec4 calcLightningColor(float taperAdj, float widthAdj, vec2 real_texcoord)
 	vec2 uv = real_texcoord;
 	uv.x *= uv_x_lightning_mult;
     uv.y *= uv_y_mult;
-	float fbmscale = 0.333;
+	float fbmscale = 1.0;
 
     float wavefrontAdj = taperAdj * widthAdj;
 
@@ -439,11 +351,11 @@ vec4 calcLightningColor(float taperAdj, float widthAdj, vec2 real_texcoord)
     uv.y *= 0;
     uv.x *= 0.8;
 
-    float noise = fbmLightning((uv * 2 * fbmscale), (current_tick + seed) * (1.0 / 30.0) * fbmscale) * 2;
+    float noise = fbm((uv * 2 * fbmscale), (current_tick + seed) * (1.0 / 30.0) * fbmscale) * 2;
     float s1 = ((wavefront_pos) + (noise * endXReduction));
     float colorGrad = 1.0 / (s1 * (50.0));
 	float alpha = abs(colorGrad * colorGrad);
-	float quadPosAlphaAdj = (uv.x + 1) / 2;
+	float quadPosAlphaAdj = (real_texcoord.x + 1) / 2;
 	vec3 color = blendVectors(secondaryColor, primaryColor, quadPosAlphaAdj);
 
 	float epsilon = 0.01;
