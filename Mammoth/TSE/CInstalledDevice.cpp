0072308795b1ee2b98b5639ae61bcede2593bb9e
Bryon Leung <exinfinitum@users.noreply.github.com>
2021-03-25 22:37:04 -0400

    Fix formatting in CInstalledDevice.cpp

--- Mammoth/TSE/CInstalledDevice.cpp
+++ Mammoth/TSE/CInstalledDevice.cpp
@@ -579,7 +579,7 @@ void CInstalledDevice::PaintDevicePos (const SDeviceDesc &Device, CG32bitImage &
 		}
 	}
 
-void CInstalledDevice::ReadFromStream (CSpaceObject &Source, SLoadCtx& Ctx)
+void CInstalledDevice::ReadFromStream (CSpaceObject &Source, SLoadCtx &Ctx)
 
 //	ReadFromStream
 //
@@ -733,36 +733,36 @@ void CInstalledDevice::ReadFromStream (CSpaceObject &Source, SLoadCtx& Ctx)
 		}
 
 	Ctx.pStream->Read(dwLoad);
-	m_fOmniDirectional = ((dwLoad & 0x00000001) ? true : false);
-	m_f3DPosition = (((dwLoad & 0x00000002) ? true : false) && (Ctx.dwVersion >= 73));
-	m_fFateSurvives = (((dwLoad & 0x00000004) ? true : false) && (Ctx.dwVersion >= 58));
-	m_fOverdrive = ((dwLoad & 0x00000008) ? true : false);
-	m_fOptimized = ((dwLoad & 0x00000010) ? true : false);
-	m_fSecondaryWeapon = ((dwLoad & 0x00000020) ? true : false);
-	m_fFateDamaged = (((dwLoad & 0x00000040) ? true : false) && (Ctx.dwVersion >= 58));
-	m_fEnabled = ((dwLoad & 0x00000080) ? true : false);
-	m_fWaiting = ((dwLoad & 0x00000100) ? true : false);
-	m_fTriggered = ((dwLoad & 0x00000200) ? true : false);
-	m_fRegenerating = ((dwLoad & 0x00000400) ? true : false);
-	m_fLastActivateSuccessful = ((dwLoad & 0x00000800) ? true : false);
+	m_fOmniDirectional =				((dwLoad & 0x00000001) ? true : false);
+	m_f3DPosition =						(((dwLoad & 0x00000002) ? true : false) && (Ctx.dwVersion >= 73));
+	m_fFateSurvives =					(((dwLoad & 0x00000004) ? true : false) && (Ctx.dwVersion >= 58));
+	m_fOverdrive =						((dwLoad & 0x00000008) ? true : false);
+	m_fOptimized =						((dwLoad & 0x00000010) ? true : false);
+	m_fSecondaryWeapon =				((dwLoad & 0x00000020) ? true : false);
+	m_fFateDamaged =					(((dwLoad & 0x00000040) ? true : false) && (Ctx.dwVersion >= 58));
+	m_fEnabled =						((dwLoad & 0x00000080) ? true : false);
+	m_fWaiting =						((dwLoad & 0x00000100) ? true : false);
+	m_fTriggered =						((dwLoad & 0x00000200) ? true : false);
+	m_fRegenerating =					((dwLoad & 0x00000400) ? true : false);
+	m_fLastActivateSuccessful =			((dwLoad & 0x00000800) ? true : false);
 
-	m_fLinkedFireAlways = ((dwLoad & 0x00001000) ? true : false);
-	m_fLinkedFireTarget = ((dwLoad & 0x00002000) ? true : false);
-	m_fLinkedFireEnemy = ((dwLoad & 0x00004000) ? true : false);
-	m_fExternal = ((dwLoad & 0x00008000) ? true : false);
-	m_fDuplicate = ((dwLoad & 0x00010000) ? true : false);
-	m_fCannotBeEmpty = ((dwLoad & 0x00020000) ? true : false);
-	m_fFateDestroyed = ((dwLoad & 0x00040000) ? true : false);
-	m_fFateComponetized = ((dwLoad & 0x00080000) ? true : false);
-	bool bSlotEnhancements = ((dwLoad & 0x00100000) ? true : false);
-	m_fLinkedFireSelected = ((dwLoad & 0x00200000) ? true : false);
-	m_fLinkedFireNever = ((dwLoad & 0x00400000) ? true : false);
-	m_fLinkedFireSelectedVariants = ((dwLoad & 0x00800000) ? true : false);
-	m_fCycleFire = ((dwLoad & 0x01000000) ? true : false);
-	m_fCanTargetMissiles = ((dwLoad & 0x02000000) ? true : false);
-	m_fOnSegment = ((dwLoad & 0x04000000) ? true : false);
-	m_fOnUsedLastAmmo = ((dwLoad & 0x08000000) ? true : false);
-	bool bLoadWeaponTargetDefinition = ((dwLoad & 0x10000000) ? true : false);
+	m_fLinkedFireAlways =				((dwLoad & 0x00001000) ? true : false);
+	m_fLinkedFireTarget =				((dwLoad & 0x00002000) ? true : false);
+	m_fLinkedFireEnemy =				((dwLoad & 0x00004000) ? true : false);
+	m_fExternal =						((dwLoad & 0x00008000) ? true : false);
+	m_fDuplicate =						((dwLoad & 0x00010000) ? true : false);
+	m_fCannotBeEmpty =					((dwLoad & 0x00020000) ? true : false);
+	m_fFateDestroyed =					((dwLoad & 0x00040000) ? true : false);
+	m_fFateComponetized =				((dwLoad & 0x00080000) ? true : false);
+	bool bSlotEnhancements =			((dwLoad & 0x00100000) ? true : false);
+	m_fLinkedFireSelected =				((dwLoad & 0x00200000) ? true : false);
+	m_fLinkedFireNever =				((dwLoad & 0x00400000) ? true : false);
+	m_fLinkedFireSelectedVariants =		((dwLoad & 0x00800000) ? true : false);
+	m_fCycleFire =						((dwLoad & 0x01000000) ? true : false);
+	m_fCanTargetMissiles =				((dwLoad & 0x02000000) ? true : false);
+	m_fOnSegment =						((dwLoad & 0x04000000) ? true : false);
+	m_fOnUsedLastAmmo =					((dwLoad & 0x08000000) ? true : false);
+	bool bLoadWeaponTargetDefinition =	((dwLoad & 0x10000000) ? true : false);
 
 	//	Previous versions did not save this flag
 
