7e44630bffbcc01852fd7367806a85ef7a6b4449
FlufflesTheMicrosaur <68170519+FlufflesTheMicrosaur@users.noreply.github.com>
2025-08-11 15:51:20 -0700

    feat: 104353: add LibJPEGTurboUtil project for x64 build with several fixes ported in from the intermediate x64 branch in the FlufflesTheMicrosaur/TranscendenceDev repo from 2021 (#161)

---
+++ Alchemy/LibJPEGTurboUtil/precomp.h
@@ -0,0 +1,14 @@
+//	PreComp.h
+//
+//	LibJPEGTurbo Precompiled Headers
+//	Copyright (c) 2022 Kronosaur Productions, LLC. All Rights Reserved.
+
+#pragma once
+
+#include "Kernel.h"
+#include "KernelObjID.h"
+using namespace Kernel;
+
+#include "Graphics.h"
+#include "turbojpeg.h"
+#include "LibJPEGTurboUtil.h"
