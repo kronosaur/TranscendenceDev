3dcc1d0920d81da612d3ce66d07604d8765e33e8
FlufflesTheMicrosaur <Fluffles.The.Microsaur@gmail.com>
2025-10-09 14:55:46 -0700

    chore: 104613 update documentation

--- Mammoth/Include/TSEVersions.h
+++ Mammoth/Include/TSEVersions.h
@@ -572,6 +572,15 @@ constexpr DWORD SYSTEM_SAVE_VERSION =					213;
 //			(bRoR x count)
 //				Returns x rotated right by count bits in 32-bit space.
 //				Bits shifted out of the low end wrap around to the high end.
+//		<AdventureDesc>
+//			<Constants>
+//				<MiningMaxOreLevels>
+//					miningMaxOreLevel: (damageAdj-style list of ints)
+//						The maximum level ore that this weapon shot can extract
+//						Specifying a "+" before a level means this is a positive
+//							offset relative to the level of the item
+//						Specifying a "-" before a level means this is a negative
+//							offset relative to the level of the item
 //		<ItemType>
 //			<Weapon>
 //				miningMaxOreLevel: (int: 0-25)
