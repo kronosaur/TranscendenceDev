53d4fb93a6da01265f91a6f309f38810e25d3391
FlufflesTheMicrosaur <68170519+FlufflesTheMicrosaur@users.noreply.github.com>
2026-01-05 11:25:50 -0800

    fix: 105788: correctly set the remaining repeating shots left before firing a repeating weapon that needs to preserve targeting information (ex, an omni or swivel) (#292)

--- Mammoth/TSE/CWeaponClass.cpp
+++ Mammoth/TSE/CWeaponClass.cpp
@@ -5966,6 +5966,8 @@ void CWeaponClass::Update (CInstalledDevice *pDevice, CSpaceObject *pSource, SDe
 
 				for (DWORD i = 0; i < dwShotsThisTick; i++)
 					{
+					ActivateCtx.iRepeatingCount = dwContinuousShots - i;
+
 					FireWeapon(*pDevice, *pShot, ActivateCtx, rContinuousInterpolationTime, i+1);
 
 					if (pSource->IsDestroyed())
